5c38348a3378a7c27dcba3f9656dc675c5faf2ec
Deep-Pajpani <pajpanideep@gmail.com>
2025-07-24 20:37:36 +0530

    Changes of json recieving from frontend

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def get_user_by_mobile():
 def approve_or_not():
     data = request.get_json()
     name = data.get('fullName')
-    mobileNumber = data.get('mobile')
+    mobileNumber = data.get('mobileNo')
     annual_income_raw = data.get('annualIncome')
     
     try:
